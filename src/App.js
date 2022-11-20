96d6084a0a0a050dda59b336d9df920628d9674b
Eray Sahin <eraysahinmoon@gmail.com>
2022-11-19 22:00:56 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import AddUser from './components/AddUser';
+import Users from './components/Users';
+
+class App extends Component {
+
+
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      users:[
+        {
+          id: 1,
+          name: "Eray Sahin",
+          email: "eraysahinmoon@gmail.com",
+        },
+        {
+          id: 2,
+          name: "Seref Test",
+          email: "erahinmoon@gmail.com",
+        },
+        {
+          id: 3,
+          name: "Eray Tester",
+          email: "eraysahn@gmail.com",
+        },
+      ]
+    };
+    this.deleteUser = this.deleteUser.bind(this);
+    this.addUser = this.addUser.bind(this);
+  }
+
+  addUser(newUser) {
+    let updatedUsers = this.state.users;
+    updatedUsers.push(newUser);
+    this.setState({
+      users: updatedUsers
+    });
+  }
+
+  deleteUser(id) {
+    let updatedUsers = this.state.users;
+    updatedUsers = updatedUsers.filter(user => user.id !== id);
+    this.setState({users: updatedUsers});
+  }
+
+    
+  render() {
+    return (
+      <div className="container">
+        <h1>React First Project - Add & Delete Users</h1>
+        <hr />
+        <AddUser addUser={this.addUser}/>
+        <hr />
+        <Users deleteUser = {this.deleteUser} users = {this.state.users}/>
+      </div>
+    )
+  }
+
+}
+
+export default App;
---
+++ src/components/AddUser.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+
+
+class AddUser extends Component{
+
+    state = {
+        name: "",
+        email: "",
+    }
+    onNameChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    onEmailChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+
+        });
+    }
+
+    onAddSubmit(e) {
+        const { name, email } = this.state;
+        const { addUser } = this.props;
+        const newUser = {
+            id: Math.floor(Math.random() * 100),
+            name,
+            email
+        }   
+        addUser(newUser);
+        e.preventDefault();
+    }
+
+    render(){
+        const { name, email } = this.state;
+        return(
+          <div className="card">
+            
+            <h4 className="card-header">Add New User</h4>
+            <div className="card-body">
+                <form onSubmit={this.onAddSubmit.bind(this)}>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" placeholder="Enter Name" className="form-control" value={name} onChange= {this.onNameChange.bind(this)}/>
+                    </div>
+                    <br />
+                    <div className="form-group">
+                        <label htmlFor="email">E-mail</label>
+                        <input type="text" name="email" id="email" placeholder="Enter E-mail" className="form-control" value={email} onChange= {this.onEmailChange.bind(this)} />
+                    </div>
+                    <br />
+                    <button type='submit' className="btn btn-danger btn-block">Add New User</button>
+
+                </form>
+
+
+
+            </div>
+
+
+
+          </div>
+        );
+    }
+}
+export default AddUser;
